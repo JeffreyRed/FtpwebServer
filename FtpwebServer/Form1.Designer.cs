ed2f581650100d56828cdcd79b7fbc02b25d2b3a
jeffred <jeffrey.05.09@gmail.com>
2020-08-10 15:33:50 +0300

    Add project files.

---
+++ FtpwebServer/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace FtpwebServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkdown = new System.Windows.Forms.CheckBox();
+            this.checkup = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(82, 100);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(82, 247);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "File Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // checkdown
+            // 
+            this.checkdown.AutoSize = true;
+            this.checkdown.Location = new System.Drawing.Point(82, 27);
+            this.checkdown.Name = "checkdown";
+            this.checkdown.Size = new System.Drawing.Size(74, 17);
+            this.checkdown.TabIndex = 10;
+            this.checkdown.Text = "Download";
+            this.checkdown.UseVisualStyleBackColor = true;
+            this.checkdown.CheckedChanged += new System.EventHandler(this.checkdown_CheckedChanged);
+            // 
+            // checkup
+            // 
+            this.checkup.AutoSize = true;
+            this.checkup.Location = new System.Drawing.Point(82, 51);
+            this.checkup.Name = "checkup";
+            this.checkup.Size = new System.Drawing.Size(60, 17);
+            this.checkup.TabIndex = 11;
+            this.checkup.Text = "Upload";
+            this.checkup.UseVisualStyleBackColor = true;
+            this.checkup.CheckedChanged += new System.EventHandler(this.checkup_CheckedChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "FileName";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(82, 336);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(343, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork_1);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 468);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkup);
+            this.Controls.Add(this.checkdown);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkdown;
+        private System.Windows.Forms.CheckBox checkup;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ FtpwebServer/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+namespace FtpwebServer
+{
+    public partial class Form1 : Form
+    {
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+
+        public string Username;
+        public string Filename;
+        public string Fullname;
+        public string Server;
+        public string Password;
+        public string path;
+        public string localdest;
+
+        public Form1()
+        {
+            InitializeComponent();
+            checkdown.Checked = true;
+            checkup.Enabled = false;
+            button1.Text = @"Download";
+            label5.Enabled = true;
+            textBox4.Enabled = true;
+            checkup.Enabled = false;
+            label4.Text = @"Downloaded 0%";
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+
+        //Background
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            if (checkdown.Checked == true)
+            {
+                label4.Text = $"Downloaded {e.ProgressPercentage}%";
+                label4.Update();
+                progressBar1.Value = e.ProgressPercentage;
+                progressBar1.Update();
+            }
+
+            if (checkup.Checked == true)
+            {
+                label4.Text = $"Uploaded {e.ProgressPercentage}%";
+                label4.Update();
+                progressBar1.Value = e.ProgressPercentage;
+                progressBar1.Update();
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (checkdown.Checked == true)
+            {
+                label4.Text = "Download Complete!";
+                MessageBox.Show("Download Complete!");
+            }
+
+            if (checkup.Checked == true)
+            {
+                label4.Text = "Upload Complete!";
+                MessageBox.Show("Upload Complete!");
+            }
+
+        }
+
+        private void checkdown_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkdown.Checked == false)
+            {
+                checkup.Enabled = true;
+                checkup.Checked = true;
+                button1.Text = @"Upload";
+                label5.Enabled = false;
+                textBox4.Enabled = false;
+                checkdown.Enabled = false;
+                label4.Text = @"Uploaded 0%";
+            }
+        }
+
+        private void checkup_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkup.Checked == false)
+            {
+                checkdown.Enabled = true;
+                checkdown.Checked = true;
+                button1.Text = @"Download";
+                label5.Enabled = true;
+                textBox4.Enabled = true;
+                checkup.Enabled = false;
+                label4.Text = @"Downloaded 0%";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (checkup.Checked == true)
+            {
+
+                using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true, ValidateNames = true, Filter = "All Files|*.*" })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        FileInfo fi = new FileInfo(ofd.FileName);
+                        Username = textBox1.Text;
+                        Password = textBox2.Text;
+                        Server = textBox3.Text;
+                        Filename = fi.Name;
+                        Fullname = fi.FullName;
+                    }
+                }
+            }
+
+
+            if (checkdown.Checked == true)
+            {
+                Username = textBox1.Text;
+                Password = textBox2.Text;
+                Server = textBox3.Text;
+                Filename = textBox4.Text;
+                path = @"C:\Users\jredondo\Documents\";
+                localdest = path + @"" + Filename;
+                Fullname = Server + @"/" + Filename;
+            }
+
+            //Start the Background and wait a little to start it.
+            Thread.Sleep(1000);
+            backgroundWorker1.RunWorkerAsync();  //the most important command to start the background worker
+            Thread.Sleep(1000);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+
+            if (checkdown.Checked == true)
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, Filename)));
+
+                request.Credentials = new NetworkCredential(Username, Password);
+                request.Method = WebRequestMethods.Ftp.DownloadFile;  //Download Method
+
+
+                //Get some data form the source file like the zise and the TimeStamp. every data you request need to be a different request and response
+                FtpWebRequest request1 = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, Filename)));
+                request1.Credentials = new NetworkCredential(Username, Password);
+                request1.Method = WebRequestMethods.Ftp.GetFileSize;  //GetFileze Method
+                FtpWebResponse response = (FtpWebResponse)request1.GetResponse();
+                double total = response.ContentLength;
+                response.Close();
+
+                FtpWebRequest request2 = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, Filename)));
+                request2.Credentials = new NetworkCredential(Username, Password);
+                request2.Method = WebRequestMethods.Ftp.GetDateTimestamp; //GetTimestamp Method
+                FtpWebResponse response2 = (FtpWebResponse)request2.GetResponse();
+                DateTime modify = response2.LastModified;
+                response2.Close();
+
+
+                Stream ftpstream = request.GetResponse().GetResponseStream();
+                FileStream fs = new FileStream(localdest, FileMode.Create);
+
+                // Method to calculate and show the progress.
+                byte[] buffer = new byte[1024];
+                int byteRead = 0;
+                double read = 0;
+                do
+                {
+                    byteRead = ftpstream.Read(buffer, 0, 1024);
+                    fs.Write(buffer, 0, byteRead);
+                    read += (double)byteRead;
+                    double percentage = read / total * 100;
+                    backgroundWorker1.ReportProgress((int)percentage);
+                }
+                while (byteRead != 0);
+                ftpstream.Close();
+                fs.Close();
+
+
+            }
+            else
+            {
+
+                //Upload Method.
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, Filename)));
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.Credentials = new NetworkCredential(Username, Password);
+                Stream ftpstream = request.GetRequestStream();
+                FileStream fs = File.OpenRead(Fullname);
+
+                // Method to calculate and show the progress.
+                byte[] buffer = new byte[1024];
+                double total = (double)fs.Length;
+                int byteRead = 0;
+                double read = 0;
+                do
+                {
+                    byteRead = fs.Read(buffer, 0, 1024);
+                    ftpstream.Write(buffer, 0, byteRead);
+                    read += (double)byteRead;
+                    double percentage = read / total * 100;
+                    backgroundWorker1.ReportProgress((int)percentage);
+                }
+                while (byteRead != 0);
+                fs.Close();
+                ftpstream.Close();
+
+            }
+        }
+    }
+}
